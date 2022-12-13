e05972fe4b1afe1b1983c80f25c923fc13a5b83f
SophTO <98454880+SophTO@users.noreply.github.com>
2022-12-13 11:43:19 +0100

    Add files via upload

---
+++ demo2.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('div1').innerHTML= "Mon nouveau texte"; 
+
+var div = document.createElement("div7");
+document.body.appendChild (div);
+
+document.getElementById("div7").innerHTML= "ma div 7 est là"; 
+
+var divSupprime = document.getElementById ('div2');
+document.body.removeChild (divSupprime);
+
+
+var monImage = document.getElementById('image1');
+monImage.style.width = '100px' ;
+
